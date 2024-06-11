e2c43eb6bdb5149624cf75a068c494145f0774db
innabatalova <batalovainna97@gmail.com>
2024-06-11 11:16:58 +0300

    Initial setting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC, ReactElement } from 'react'
+
+import styles from './App.module.scss'
+
+const App: FC = (): ReactElement => {
+  return (
+    <div className={styles.App}>
+    </div>
+  );
+}
+
+export default App
